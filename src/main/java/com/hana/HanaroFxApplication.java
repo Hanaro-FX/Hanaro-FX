7f1dd80676d3aef09a043821b17437490a342177
soyeonvv <sovvworld@gmail.com>
2024-04-22 16:37:50 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/hana/HanaroFxApplication.java
@@ -0,0 +1,13 @@
+package com.hana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanaroFxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanaroFxApplication.class, args);
+    }
+
+}
