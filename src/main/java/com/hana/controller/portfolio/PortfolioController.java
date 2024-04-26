d4777e44db2d7b57fea3a693799b5ebe00facddb
Kwak-JunYoung <klop9090@g.skku.edu>
2024-04-26 10:38:05 +0900

    Feat: Initialize portfolio controller

---
+++ src/main/java/com/hana/controller/portfolio/PortfolioController.java
@@ -0,0 +1,14 @@
+package com.hana.controller.portfolio;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/portfolio")
+@Slf4j
+public class PortfolioController {
+
+}
