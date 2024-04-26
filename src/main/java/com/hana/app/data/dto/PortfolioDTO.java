10b23527cf1845feba90234e20eec79a9614773e
Kwak-JunYoung <klop9090@g.skku.edu>
2024-04-26 10:22:48 +0900

    Feat: Addition of portfolio DT

---
+++ src/main/java/com/hana/app/data/dto/PortfolioDTO.java
@@ -0,0 +1,52 @@
+package com.hana.app.data.dto;
+
+import java.time.LocalDate;
+
+public class PortfolioDTO {
+    private int id;
+    private int userId;
+    private String portfolioName;
+    private String portfolioDesc;
+    private LocalDate portfolioDate;
+
+    //    0, 1, 3, 6, 12
+    private int rebalancing;
+    
+    private long percentage_AED;
+    private long percentage_ARS;
+    private long percentage_AUD;
+    private long percentage_BHD;
+    private long percentage_CAD;
+    private long percentage_CHF;
+    private long percentage_CNY;
+    private long percentage_DKK;
+    private long percentage_EUR;
+    private long percentage_GBP;
+    private long percentage_HKD;
+    private long percentage_HUF;
+    private long percentage_IDR;
+    private long percentage_ILS;
+    private long percentage_INR;
+    private long percentage_JPY;
+    private long percentage_KWD;
+    private long percentage_MXN;
+    private long percentage_MYR;
+    private long percentage_NOK;
+    private long percentage_NZD;
+    private long percentage_PHP;
+    private long percentage_PKR;
+    private long percentage_PLN;
+    private long percentage_QAR;
+    private long percentage_RUB;
+    private long percentage_SAR;
+    private long percentage_SEK;
+    private long percentage_SGD;
+    private long percentage_THB;
+    private long percentage_TRY;
+    private long percentage_TWD;
+    private long percentage_USD;
+    private long percentage_VND;
+    private long percentage_XAG;
+    private long percentage_XAU;
+    private long percentage_ZAR;
+}
