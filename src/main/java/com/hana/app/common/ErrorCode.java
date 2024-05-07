7d8c802782d7aae335dae31ef37a9dfe0f5f6551
soyeonvv <sovvworld@gmail.com>
2024-05-07 16:56:55 +0900

    feat: User 예외 처리

--- src/main/java/com/hana/app/common/ErrorCode.java
+++ src/main/java/com/hana/app/common/ErrorCode.java
@@ -11,6 +11,7 @@ import static org.springframework.http.HttpStatus.*;
 @Getter
 public enum ErrorCode {
 
+    INVALID_TOKEN(UNAUTHORIZED, "유효하지 않은 토큰"),
     TOKEN_EXPIRED(UNAUTHORIZED, "토큰이 만료되었습니다."),
     USER_NOT_EXIST(NOT_FOUND, "존재하지 않는 회원"),
     NOT_PERMISSION(UNAUTHORIZED, "권한이 없습니다."),
