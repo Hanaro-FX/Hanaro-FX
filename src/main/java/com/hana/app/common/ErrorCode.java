caddc3c24ff7a871e531d69f21ed0dfe5efbf491
soyeonvv <sovvworld@gmail.com>
2024-05-07 17:35:04 +0900

    feat: 포트폴리오 예외 처리

--- src/main/java/com/hana/app/common/ErrorCode.java
+++ src/main/java/com/hana/app/common/ErrorCode.java
@@ -12,9 +12,7 @@ import static org.springframework.http.HttpStatus.*;
 public enum ErrorCode {
 
     INVALID_TOKEN(UNAUTHORIZED, "유효하지 않은 토큰"),
-    TOKEN_EXPIRED(UNAUTHORIZED, "토큰이 만료되었습니다."),
     USER_NOT_EXIST(NOT_FOUND, "존재하지 않는 회원"),
-    NOT_PERMISSION(UNAUTHORIZED, "권한이 없습니다."),
 
     PORTFOLIO_NOT_EXIST(NOT_FOUND, "존재하지 않는 포트폴리오"),
     USER_NOT_HAVE_PORTFOLIO(BAD_REQUEST, "회원이 가진 포트폴리오가 아닙니다."),
